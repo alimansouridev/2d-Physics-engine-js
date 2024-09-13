e5e73335a0626fe3d8733159fce76d5630be5f3a
elijuggz <alimansouri.dev@gmail.com>
2024-09-12 20:28:21 -0400

    Easier way found to make this using the canvas html element as render

---
+++ engine.js
@@ -0,0 +1,24 @@
+//store the canvas element in a variable
+const canvas = document.getElementById("canvas");
+//define the rendering context
+const ctx = canvas.getContext("2d");
+
+//a function that draws a circle and fills it with a color
+function drawBall(x, y, r) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.strokeStyle = "black";
+  ctx.stroke();
+  ctx.fillStyle = "blue";
+  ctx.fill();
+  ctx.closePath();
+}
+
+canvas.addEventListener("keydown", function () {
+  console.log("ayo");
+});
+
+//calling the function twice
+drawBall(100, 100, 20);
+
+drawBall(200, 200, 30);
